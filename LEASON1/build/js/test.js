c3bad0fecb2a42ab0e18945d5c4560ab4f5e010e
salamanderctesiphon <swbrookshire@gmail.com>
2024-01-12 16:13:22 -0600

    I need to have my head examined

--- LEASON1/build/js/test.js
+++ LEASON1/build/js/test.js
@@ -4,3 +4,8 @@ let anny = 'this is an any';
 let man = true;
 let woman = false;
 let num = 1000;
+let numQ = 100000;
+let num4 = 40000;
+if (numQ > num) {
+    console.log('You have done a great job You bumbling idiot!!!');
+}
--- LEASON1/src/test.ts
+++ LEASON1/src/test.ts
@@ -3,3 +3,9 @@ let anny = 'this is an any'
 let man = true
 let woman = false
 let num = 1000
+let numQ = 100000
+let num4 = 40000
+
+if (numQ > num) {
+  console.log('You have done a great job You bumbling idiot!!!')
+}
