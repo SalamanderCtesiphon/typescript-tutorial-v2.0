a074dc5d8d272b42cf8621d9a373214f2e87292e
salamanderctesiphon <swbrookshire@gmail.com>
2024-01-12 17:36:44 -0600

    following the lesson

--- LEsSON3/build/js/main.js
+++ LEsSON3/build/js/main.js
@@ -1,2 +1,21 @@
 "use strict";
-let myName = 'Shannon';
+let stringArray = ['one', 'hey', 'Dave'];
+let guitars = ['Strat', 'Les Paul', 5150];
+let mixedDAta = ['EVH', 1984, true];
+stringArray[0] = 'John';
+stringArray.push('hey');
+guitars[0] = 1984;
+console.log(stringArray);
+guitars.unshift('Jim');
+let test = [];
+let bands = [];
+bands.push('Van Halen');
+// Tuple
+let myTuple = ['Dave', 42, true];
+let mixed = ['John', 1, false];
+mixed = myTuple;
+myTuple[1] = 42;
+// Objects 
+let myObj;
+myObj = [];
+console.log(typeof myObj);
--- LEsSON3/src/main.ts
+++ LEsSON3/src/main.ts
@@ -1,3 +1,30 @@
 let stringArray =  ['one', 'hey', 'Dave']
 let guitars = ['Strat', 'Les Paul', 5150]
 let mixedDAta = ['EVH', 1984, true]
+
+stringArray[0] = 'John'
+stringArray.push('hey')
+guitars[0] = 1984
+
+console.log(stringArray)
+guitars.unshift('Jim')
+
+let test = []
+let bands: string[] = []
+bands.push('Van Halen')
+
+// Tuple
+
+let myTuple: [string, number, boolean] = ['Dave', 42, true]
+
+let mixed = ['John', 1, false]
+
+mixed = myTuple
+
+myTuple[1] = 42
+
+// Objects 
+
+let myObj: object
+myObj = []
+console.log(typeof myObj)
