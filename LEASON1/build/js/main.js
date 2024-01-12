81cd1c6796d0aa6a63a604b6d2a313d9b4129cda
salamanderctesiphon <swbrookshire@gmail.com>
2024-01-12 16:15:00 -0600

    coding is fun, but not profitable

--- LEASON1/build/js/main.js
+++ LEASON1/build/js/main.js
@@ -7,3 +7,13 @@ console.log(username);
 let a = 12;
 let b = '6';
 let c = 2;
+let test = 'one two';
+let anny = 'this is an any';
+let man = true;
+let woman = false;
+let num = 1000;
+let numQ = 100000;
+let num4 = 40000;
+if (numQ > num) {
+    console.log('You have done a great job You bumbling idiot!!!');
+}
