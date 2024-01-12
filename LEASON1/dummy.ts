c049a8961b782be9b0b317ee2c6cac95b7e3436e
salamanderctesiphon <swbrookshire@gmail.com>
2024-01-12 16:17:09 -0600

    tom fooleryy

---
+++ LEASON1/dummy.ts
@@ -0,0 +1,6 @@
+let anArray = [
+  'this is the first item and it can be found at index zero;',
+  'this is a second item',
+  'and another item',
+  'I wpmder of tjoso wo; I wonder if this will help me build my app>?'
+]
