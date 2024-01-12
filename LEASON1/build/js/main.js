ceb796651a82edabf84bcecdc80b65f1e5f4510a
salamanderctesiphon <swbrookshire@gmail.com>
2024-01-12 15:45:15 -0600

    goofing around

--- LEASON1/build/js/main.js
+++ LEASON1/build/js/main.js
@@ -1,3 +1,6 @@
 "use strict";
 let username = 'Shannon';
 console.log(username);
+// trying to get the display to show ts
+// I wonder how much text that will rerquire
+// Let's test it out
--- LEASON1/src/main.ts
+++ LEASON1/src/main.ts
@@ -1,2 +1,6 @@
 let username = 'Shannon'
 console.log(username)
+
+// trying to get the display to show ts
+// I wonder how much text that will rerquire
+// Let's test it out
