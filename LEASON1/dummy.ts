13602ed01c812a2789f9a2c9c87022e86945efb9
salamanderctesiphon <swbrookshire@gmail.com>
2024-01-12 16:30:19 -0600

    goofing around still i need help with my finances

--- LEASON1/dummy.ts
+++ LEASON1/dummy.ts
@@ -4,3 +4,25 @@ let anArray = [
   'and another item',
   'I wpmder of tjoso wo; I wonder if this will help me build my app>?'
 ]
+let username2 = 'Shannon'
+console.log(username)
+
+// trying to get the display to show ts
+// I wonder how much text that will rerquire
+// Let's test it out
+
+let aa = 12
+let ba = '6'
+let ca = 2
+
+let test = 'one two'
+let anny = 'this is an any'
+let man = true
+let woman = false
+let num = 1000
+let numQ = 100000
+let num4 = 40000
+
+if (numQ > num) {
+  console.log('You have done a great job You bumbling idiot!!!')
+}
--- LEASON1/src/main.ts
+++ LEASON1/src/main.ts
@@ -1,22 +1,8 @@
-let username = 'Shannon'
+let username = "Shannon"
 console.log(username)
-
-// trying to get the display to show ts
-// I wonder how much text that will rerquire
-// Let's test it out
 
 let a = 12
 let b = '6'
 let c = 2
 
-let test = 'one two'
-let anny = 'this is an any'
-let man = true
-let woman = false
-let num = 1000
-let numQ = 100000
-let num4 = 40000
-
-if (numQ > num) {
-  console.log('You have done a great job You bumbling idiot!!!')
-}
+console.log(a / b)
