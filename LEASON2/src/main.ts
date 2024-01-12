7b9d7854df32aacbd5dc2379ebdbf8e2dab63008
salamanderctesiphon <swbrookshire@gmail.com>
2024-01-12 17:21:30 -0600

    following the lessons

--- LEASON2/src/main.ts
+++ LEASON2/src/main.ts
@@ -1,14 +1,19 @@
 let myName: string
 let meaningOfLife: number
 let isLoading: boolean
-let album: any 
+let album: string | number
 
 myName = 'Shannon'
 meaningOfLife = 42
 isLoading = true
 
-album = true
+album = 1984
 
-const sum = (a, b) => {
+const sum = (a: number, b: number) => {
   return a + b
 }
+
+let postId: string | number
+let isActive: number | boolean
+
+let re: RegExp = /|w+/g
