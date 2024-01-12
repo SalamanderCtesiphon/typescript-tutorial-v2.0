ba317cdf7829a4c60155b0c6339a6e16c608f056
salamanderctesiphon <swbrookshire@gmail.com>
2024-01-12 16:10:48 -0600

    tring to displayy ts on the git hub page

--- LEASON1/build/js/test.js
+++ LEASON1/build/js/test.js
@@ -1,2 +1,6 @@
 "use strict";
 let test = 'one two';
+let anny = 'this is an any';
+let man = true;
+let woman = false;
+let num = 1000;
--- LEASON1/src/test.ts
+++ LEASON1/src/test.ts
@@ -1,1 +1,5 @@
 let test = 'one two'
+let anny = 'this is an any'
+let man = true
+let woman = false
+let num = 1000
