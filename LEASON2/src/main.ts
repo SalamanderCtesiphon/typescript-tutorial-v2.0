e0d189c859adc3562656858d26fb73a6fc81885e
salamanderctesiphon <swbrookshire@gmail.com>
2024-01-12 16:48:30 -0600

    working throught the examples

--- LEASON2/src/main.ts
+++ LEASON2/src/main.ts
@@ -1,1 +1,14 @@
-let myName: string = 'Shannon'
+let myName: string
+let meaningOfLife: number
+let isLoading: boolean
+let album: any 
+
+myName = 'Shannon'
+meaningOfLife = 42
+isLoading = true
+
+album = true
+
+const sum = (a, b) => {
+  return a + b
+}
